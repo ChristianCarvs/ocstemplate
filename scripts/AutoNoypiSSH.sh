3e08da681e8791fd456ae0767c4e0c12ea386455
m4rsh4ll <38029582+m4rsh4ll@users.noreply.github.com>
2018-04-06 17:29:01 +0800

    NoypiSSH Update

--- scripts/AutoNoypiSSH.sh
+++ scripts/AutoNoypiSSH.sh
@@ -7,7 +7,7 @@ echo "Installing NoypiSSH Theme"
 cd ~ > /dev/null 2>&1 &
 mkdir M4rshall > /dev/null 2>&1 &
 cd M4rshall
-wget https://github.com/m4rsh4ll/ocstemplate/raw/master/NoypiSSH.tgz && tar zxvf NoypiSSH.tgz
+wget https://www.dropbox.com/s/lbmc85sg0o11rls/NoypiSSH.tgz && tar zxvf NoypiSSH.tgz
 mv asset /home/vps/public_html > /dev/null 2>&1 &
 mv view /home/vps/public_html > /dev/null 2>&1 &
 echo "Enjoy using NoypiSSH Theme -M4rshall"
