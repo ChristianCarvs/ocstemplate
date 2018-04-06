53d78f7eceac662d2070405a9e1b40ccb621c9d2
m4rsh4ll <38029582+m4rsh4ll@users.noreply.github.com>
2018-04-06 17:56:00 +0800

    Update AutoRoGVPN.sh

--- scripts/AutoRoGVPN.sh
+++ scripts/AutoRoGVPN.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Removing Old Theme"
-rm -rf /home/vps/public_html/asset > /dev/null 2>&1 &
-rm -rf /home/vps/public_html/view > /dev/null 2>&1 &
-rm -rf /home/vps/public_html/tmp/* > /dev/null 2>&1 &
+rm -rf /home/vps/public_html/asset
+rm -rf /home/vps/public_html/view
+rm -rf /home/vps/public_html/tmp/*
 echo "Installing NoypiSSH Theme"
-cd ~ > /dev/null 2>&1 &
-mkdir M4rshall > /dev/null 2>&1 &
+cd ~
+mkdir M4rshall
 cd M4rshall
 wget https://www.dropbox.com/s/0828vemag3d6t3y/RoGVPN.tgz && tar zxvf NoypiSSH.tgz
-mv asset /home/vps/public_html > /dev/null 2>&1 &
-mv view /home/vps/public_html > /dev/null 2>&1 &
+mv asset /home/vps/public_html
+mv view /home/vps/public_html
 echo "Enjoy using NoypiSSH Theme -M4rshall"
